21245ba4d6a9c17b8b30d83a40e4084721d25eab
Sławomir Batruch <batruchs@gmail.com>
2023-03-18 13:24:29 +0100

    Replaced .format with f-strings where possible

--- test_VM_commands.py
+++ test_VM_commands.py
@@ -26,7 +26,7 @@ def connect(ip_addr: str, *, username: str, key_path: str) -> fabric.Connection:
         c.open()  # Try opening the connection specified above
         c.close()  # Will not be executed if the connection can not be made
     except TimeoutError:  # Raised by c.open() if connect_timeout is reached
-        logging.exception("Connection to {} not established. Reason: timed out.".format(ip_addr))
+        logging.exception(f"Connection to {ip_addr} not established. Reason: timed out.")
         exit(1)
         # TODO - Is exit really the best behavior here?
     return c
@@ -43,7 +43,6 @@ def execute(target_con: fabric.Connection, *, command: str, sudo: bool = False) 
     :param sudo: bool
     :return: fabric.Result
     """
-    print("executing {}".format(command))
     out = "Executed {0.command!r} on {0.connection.host}, got output \n{0.stdout}execution code {0.return_code}\n"
     out_err = "Error during execution of {0.command!r} on {0.connection.host}.\n" \
               "Got output on stderr \n{0.stderr}error code {0.return_code}\n"
@@ -80,9 +79,9 @@ def sudo_put_file(target_con: fabric.Connection, local_path: str, dest_path: str
 
     # Exceptions are already handled in the called functions
     put_file(target_con, local_path, temp_file, permissions=permissions, overwrite=True)  # Overwrite temp file
-    execute(target_con, command="install -o root -g root -m {} {} {}".format(permissions, temp_file, dest_path), sudo=True)
+    execute(target_con, command=f"install -o root -g root -m {permissions} {temp_file} {dest_path}", sudo=True)
 
-    execute(target_con, command="rm {}".format(temp_file))
+    execute(target_con, command=f"rm {temp_file}")
 
 
 def put_file(target_con: fabric.Connection, local_path: str, dest_path: str, *,
@@ -103,7 +102,7 @@ def put_file(target_con: fabric.Connection, local_path: str, dest_path: str, *,
     """
     try:
         if not overwrite:  # We need to check if file exists already on target
-            file_list = execute(target_con, command='find {} -maxdepth 1 -type f'.format(dest_path), sudo=True)
+            file_list = execute(target_con, command=f'find {dest_path} -maxdepth 1 -type f', sudo=True)
             if dest_path in file_list.stdout.split():  # Find command has found a file
                 raise FileExistsError("Overwrite flag was not set, but file already exists on target machine!")
         if sudo:  # Invoke a special function if we want to put the file as root
@@ -111,13 +110,13 @@ def put_file(target_con: fabric.Connection, local_path: str, dest_path: str, *,
         else:
             # Transfer the file with put
             target_con.put(local_path, dest_path)  # Might throw permission error if attempt to transfer folder is made
-            target_con.run('chmod {perm} {path}'.format(perm=permissions, path=dest_path))
+            target_con.run(f'chmod {permissions} {dest_path}')
 
     except (FileNotFoundError, FileExistsError) as e:  # General error raised if transfer fails
-        logging.exception("File related error occurred while transferring {}\nReason: {}".format(local_path, e))
+        logging.exception(f"File related error occurred while transferring {local_path}\nReason: {e}")
     except OSError:
         logging.exception(
-            "OSError occured while transferring {}. Most likely a try to transmit a folder was done".format(local_path))
+            f"OSError occured while transferring {local_path}. Most likely a try to transmit a folder was done")
     except invoke.UnexpectedExit:
         logging.exception("Error while executing find command in put_file. Check previous exception",
                           exc_info=False)
@@ -142,7 +141,7 @@ def get_default_configs(target_con: fabric.Connection, dest_path: str, type: str
     """
     try:
         if os.path.exists(dest_path) and not overwrite:
-            raise FileExistsError("Overwrite flag was not set, but files exist in {}".format(dest_path))
+            raise FileExistsError(f"Overwrite flag was not set, but files exist in {dest_path}")
         if type.lower() not in ("open5gs", "ueransim"):
             raise ValueError("Invalid type variable passed. Should be open5gs or ueransim")
     except (FileExistsError, ValueError) as e:
@@ -177,7 +176,7 @@ def get_file(target_con: fabric.connection, remote_path: str, dest_path: str = "
     try:  # If the file or folder in remote_path doesnt exist, the exception is handled in the execute function
         if folder_mode:  # We need to fetch folder contents to transfer files one by one
             # Find only files (not folders) in the specified remote_path
-            file_list = execute(target_con, command='find {} -maxdepth 1 -type f'.format(remote_path))
+            file_list = execute(target_con, command=f'find {remote_path} -maxdepth 1 -type f')
             if len(file_list.stdout) == 0:
                 raise ValueError(file_list.stderr)
             # Split each file path into different array indexes
@@ -185,13 +184,13 @@ def get_file(target_con: fabric.connection, remote_path: str, dest_path: str = "
             # Since find gives the full path, we need to extract only the file names to properly specify the destination
             file_names = [file.split("/")[-1] for file in file_list]
             for file_path, file_name in zip(file_list, file_names):
-                dest_folder = "./transfers/{}/{}".format(dest_path, file_name)
+                dest_folder = f"./transfers/{dest_path}/{file_name}"
                 target_con.get(file_path, dest_folder)
         else:
             file_name = remote_path.split("/")[-1]
-            target_con.get(remote_path, "./transfers/{}/{}".format(dest_path, file_name))
+            target_con.get(remote_path, f"./transfers/{dest_path}/{file_name}")
     except ValueError:
-        logging.exception("Transfer failed: unable to fetch file list for {}".format(remote_path))
+        logging.exception(f"Transfer failed: unable to fetch file list for {remote_path}")
         return
 
 
@@ -203,26 +202,27 @@ def install_sim(target_con: fabric.Connection, sim_name: str) -> None:
     :param sim_name: str
     :return: None
     """
+    sim_name = sim_name.lower()
     try:
-        if sim_name.lower() not in ("ueransim", "open5gs"):
+        if sim_name not in ("ueransim", "open5gs"):
             raise ValueError("Install_sim called with invalid simulator type to install. Aborting installation")
     except ValueError as e:
         logging.exception(e)
 
-    message = "Transfer of file {} to dest {} on machine {}"
-    src_path = "./scripts/install_{}.sh".format(sim_name.lower())  # dot specifies relative path
+    src_path = f"./scripts/install_{sim_name}.sh"  # dot specifies relative path
     # Due to internals of put command, we need full path. Tilde (home) won't work
     if target_con.user != "root":
-        dest_path = "/home/{}/install_{}".format(target_con.user, sim_name.lower())
+        dest_path = f"/home/{target_con.user}/install_{sim_name}"
     else:
-        dest_path = "/root/install_{}".format(sim_name.lower())
+        dest_path = f"/root/install_{sim_name}"
 
     result_path = put_file(target_con, src_path, dest_path, permissions="744", overwrite=True)  # mod: rwxr--r--
+    message = f"Transfer of file {src_path} to dest {dest_path} on machine {target_con.host}"
     if len(result_path) != 0:
-        logging.info(message.format(src_path, dest_path, target_con.host) + " successful!")
+        logging.info(message + " successful!")
     else:
         # Logging might not be needed as message is already written out in the transfer_file function
-        logging.error(message.format(src_path, dest_path, target_con.host) + " FAILED!")
+        logging.error(message + " FAILED!")
         return
     # Sudo true is needed in case connection is for the non-root user
     # However, if "no password sudo" is not enabled, this will not work for non-root
@@ -238,7 +238,7 @@ def setup_end(ip_addr: str, key_path: str) -> None:
     :return: None
     """
     print("Machine at address {} has completed setup\nAccess it with the following command:")
-    print("ssh -i {key} <username>@{IP}".format(key=key_path, IP=ip_addr))
+    print(f"ssh -i {key_path} <username>@{ip_addr}")
     print("Note: Key path can be relative")
     print("Note 2: If issues arise with bad key security error from ssh command, use windows cmd commands:")
     print("icacls <private_key_path> /inheritance:r\n icacls <private_key_path> /grant:r \"%username%\":\"(R)\"")
