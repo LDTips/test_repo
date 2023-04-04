b977116a4c00ecebe09c8091589cb012e7a4bcc5
Sławomir Batruch <batruchs@gmail.com>
2023-04-04 14:47:07 +0200

    Significant progress in start_sim

--- scripts/start_sim.sh
+++ scripts/start_sim.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
-# TODO - Globally replace echos with real commands (echo is used for testing only)
 
+# This script is not idiot proof. It's assumed the inputs are correct since they are mostly generated from Python
+# "Make something idiot proof, they'll make a better idiot" ~Unknown
+# TODO - Globally replace echos with real commands (echo is used for testing only)
 if [[ $EUID -ne 0 ]]; then
   echo "Root privileges needed" 1>&2
   exit 1
@@ -13,7 +15,7 @@ read_stream () {
   # $1 - data stream to split
   while IFS=$'\n' read -ra TEMP; do
     while IFS=' ' read -ra TEMP2; do
-        if [ -z "${TEMP2[0]}" ]; then # Trailing newline removal
+        if [ -z "${TEMP2[0]}" ]; then # Don't process empty lines
           continue
         fi
         worker_result["${TEMP2[0]}"]="${TEMP2[1]}"
@@ -36,37 +38,49 @@ echo "Detected $mode mode based on the provided text file $1"
 # Perform UERANSIM specific commands to start the UE and gNb simulators
 if [[ "$mode" == "UERANSIM" ]]; then
   # Fetch sudo caller home dir. Note - SUDO_UID is preserved even in case normal user uses sudo -s to run commands
-  SRC_USERNAME=$(id -nu "$SUDO_UID")
-  SRC_PATH=$(eval echo "~$SRC_USERNAME/UERANSIM")
+  SRC_USERNAME=$(id -nu "${SUDO_UID}")
+  SRC_PATH=$(getent passwd "${SRC_USERNAME}" | cut -d":" -f6)
   # Check callee home folder, then root home folder. If in neither UERANSIM exists, exit
-  echo "cd ${SRC_PATH}/UERANSIM" || echo "cd $HOME/UERANSIM" || echo "exit 1"
+  echo "cd ${SRC_PATH}/UERANSIM" || echo "cd ${HOME}/UERANSIM" || echo "exit 1"
 
   for key in "${!worker_result[@]}"; do
-    echo "build nr-$key -c ${worker_result[$key]} &"
+    echo "build nr-${key} -c ${worker_result[${key}]} &"
   done
-# Perform Open5Gs specific commands - more to do
+# Perform Open5Gs specific commands - more to do than in the UERANSIM case
 else
+  # Check the availability of yq. Install it if not found. We need it only for Open5Gs UPF
+  if ! yq &> /dev/null && [[ -v ${worker_result["upf"]} ]]; then
+    apt-get update
+    apt-get install yq -y
+  fi
+
   # Start daemons
   for key in "${!worker_result[@]}"; do
-    if [[ -z "${worker_result[$key]}" ]]; then
+    if [[ -z "${worker_result[${key}]}" ]]; then
       echo "/bin/open5gs-${key}d &"
     else
-      echo "/bin/open5gs-${key}d -c ${worker_result[$key]} &"
+      echo "/bin/open5gs-${key}d -c ${worker_result[${key}]} &"
     fi
   done
-  # Enable NAT and port forwards for Open5Gs related inferfaces:
-  echo "sudo sysctl -w net.ipv4.ip_forward=1"  # Ip forwarding
-  unset worker_result; declare -A worker_result  # Reset the variable to fetch different data
-  ip a | awk '/inet / && $NF!~/lo/ {print $NF " " $2}' | tee temp_file  # Fetch interfaces and their IPs, except for lo (TODO change lo to ogstun)
+  sleep 5  # Give some time for daemons and interfaces to start
+
+  # Enable NAT and port forwards for Open5Gs related interfaces:
+  echo "sudo sysctl -w net.ipv4.ip_forward=1"
+  if [[ -z "${worker_result["upf"]}" ]]; then
+    # Point to default config if it wasn't specified for the purpose getting the path in yq
+    worker_result["upf"]="/etc/open5gs/upf.yaml"
+  fi
+  # Read from upf.subnet array fields that have addr and dev field. Print in format "addr dev" to a file
+  # This is required for NAT rules for Open5Gs tunnel interfaces
+  # File is required because of how read_stream is implemented
+  yq '.upf.subnet[] | select(has("addr") and has("dev")) | .addr + " " + .dev' < "${worker_result["upf"]}" > temp_file
+  unset worker_result; declare -A worker_result  # From this point worker_result will be interface, not daemon info
   read_stream "temp_file"
+  rm -f "temp_file"
   for key in "${!worker_result[@]}"; do
-    echo "sudo iptables -t nat -A postrouting -s ${key} ! -o ${worker_result[$key]} -j MASQUERADE"  # NAT
+    echo "ip tuntap add name ${key} mode tun" 2> /dev/null # If interface exists err will be printed, but that's not an issue
+    echo "ip addr add ${worker_result[${key}]} dev ${key}"
+    echo "ip link set ${key} up"
+    echo "sudo iptables -t nat -A POSTROUTING -s ${key} ! -o ${worker_result[${key}]} -j MASQUERADE"  # NAT
   done
 fi
-
-for i in "${!worker_result[@]}"; do
-  echo "key: $i ; val: ${worker_result[$i]}"
-done
-
-
-
